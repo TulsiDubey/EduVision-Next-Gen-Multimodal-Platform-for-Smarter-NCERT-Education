86c5e4b34498528be0285d29a381344bad26acc3
TulsiDubey <dubeytulsi3@gmail.com>
2025-09-15 22:04:03 +0530

    backend folder

--- firebase.js
+++
@@ -1,8 +0,0 @@
-const fallbackConfig = {
-    apiKey: "AIzaSyDqVUpiBK9ai33MBz_-RosYtCUimXfYAL0",
-    authDomain: "ncertvisualizationtool.firebaseapp.com",
-    projectId: "ncertvisualizationtool",
-    storageBucket: "ncertvisualizationtool.appspot.com",
-    messagingSenderId: "154708621831",
-    appId: "1:154708621831:web:9f532b2004e212958608a2"
-  };
